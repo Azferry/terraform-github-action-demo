96793e0090896ad49608fc4886545e67c6fe0e70
Ryan Ferry <ryanferry@microsoft.com>
2022-09-21 13:37:09 -0400

    Rm NSG rule

--- infa/tf_deploy_network/main.tf
+++ infa/tf_deploy_network/main.tf
@@ -27,20 +27,6 @@ resource "azurerm_network_security_group" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_network_security_rule" "example" {
-  name                        = "Outbound-rule"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-
 resource "azurerm_network_security_rule" "bad_example" {
   direction                   = "Outbound"
   priority                    = 101
